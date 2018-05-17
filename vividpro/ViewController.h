288e7d3218266d85a030360015a64dc838616720
Alan Wong <vinhluan1994@gmail.com>
2018-05-18 00:29:18 +1000

    Initial Commit

---
+++ vividpro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vividpro
+//
+//  Created by Alan on 18/5/18.
+//  Copyright © 2018 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
